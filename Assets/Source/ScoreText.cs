a80675353911686be02122757efc25a82b41701d
Alsorra <alicethorntonsmith@gmail.com>
2024-02-25 17:13:57 +0100

    update script name

--- Assets/Source/ScoreText.cs
+++ Assets/Source/ScoreText.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreUI : MonoBehaviour {
+public class ScoreText : MonoBehaviour {
     void Start() {
         TMPro.TMP_Text text = GetComponent<TMPro.TMP_Text>();
         Game.instance.onScoreUpdated.AddListener((int score) => {
